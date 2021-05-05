565681381432dd76729a15a702edde55206ced5e
regynnalldo <reginaldo.sa7@gmail.com>
2021-05-04 23:36:41 -0200

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department (1,"books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(1, "lula", "lua@gh.com.br", new Date(), 5000.00, obj);
+		
+		System.out.println(seller);
 
 	}
 
